6de7eadfe1f271f017cbe63999c8403b198e98cd
jodo993 <josephdo993@yahoo.com>
2023-10-29 22:46:12 -0700

    v1.2
    - ready to test code

--- Pokemon Shop Reward Program.py
+++ Pokemon Shop Reward Program.py
@@ -8,12 +8,15 @@ def chanceToWin():
     
     # Odds of winning each prizes
     prize = {
-        "Mew EX SIR": 2000,
-        "Mew Metal Card": 1500,
-        "Mewtwo IR": 1000,
+        #"Mew EX SIR": 2000,
+        #"Mew Metal Card": 1500,
+        #"Mewtwo IR": 1000,
         "Full Art": 500,
         "Full Art V": 250,
-        "Half ex": 50,
+        "Mini Tin": 200,
+        "Energy Pack": 100,
+        "EX Card": 50,
+        "Poster Collection": 25,
         "Holo": 10,
         "Reverse Holo": 5
         }
@@ -25,28 +28,42 @@ def chanceToWin():
 
         # Has the user won?
         win = False
-
+        
         # Let user know what they won
         if userNumber == winningNumber:
             gameResultAnnoucement()
             print("********************")
             print("**                             **")
-            print("**       YOU WIN A       **")
+            print("**     YOU WIN A(N)     **")
             print("**                             **")
             print("********************")
             time.sleep(1.5)
-            print("        ", key.upper() ,"        ")
+            print("--------------------------")
+            print(key.upper())
+            print("--------------------------")
             win = True
             break
             
     # Let user know they did not win
     if(win == False):
             gameResultAnnoucement()
-            print("Sorry, you did not win this time.")        
+            print("Sorry, you did not win this time.")
+
+    goAgain = input("Press Enter to go again: ")
+    if(goAgain == ""):
+        chanceToWin()
+    else:
+        print("See you next time!")
 
 def gameResultAnnoucement():
 
     # Anticipation text before result is shown
+    print("Shuffling...........")
+    time.sleep(0.2)
+    print("Shuffling.............")
+    time.sleep(0.1)
+    print("Drawing..........")
+    time.sleep(0.3)
     print("--------------------------")
     time.sleep(0.5)
     print("            RESULT IS           ")
